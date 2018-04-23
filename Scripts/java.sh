e85394fbe76490a27e790593bc97ea313d3444e5
segstep <g.nikolov359@gmail.com>
2018-04-23 01:06:42 +0100

    Readmes scripts configs tweaks

--- Scripts/java.sh
+++ Scripts/java.sh
@@ -2,7 +2,7 @@
 
 yum -y install wget
 cd /opt/
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u161-b12/2f38c3b165be4555a1fa6e98c45e0808/jdk-8u161-linux-x64.tar.gz"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.tar.gz"
 tar xzf jdk-8u161-linux-x64.tar.gz
 alternatives --install /usr/bin/java java /opt/jdk1.8.0_161/bin/java 2
 alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_161/bin/jar 2
